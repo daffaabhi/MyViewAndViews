ac28d70a1b557510413e40ca3f48f9d38a72b059
daffaabhi <widategar@gmail.com>
2020-10-21 11:50:11 +0700

    first commit

---
+++ app/src/main/java/com/stematel/daffa/myviewandviews/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.stematel.daffa.myviewandviews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.title = "Google Pixel"
+    }
+}
